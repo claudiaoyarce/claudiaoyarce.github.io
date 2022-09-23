589cfb81f77be1cfc7adf193f501abf34d043994
claudiaoyarce <c_oyarce_u@hotmail.com>
2022-09-23 19:04:49 -0300

    cambio quinto

---
+++ assets/js/fuentesJs.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+    $(".iconos").click(function(){
+        //alert("prueba");
+        $('#tarjeta1').toggle();
+        $('#tarjeta2').toggle();
+        $('#tarjeta3').toggle();
+        //$("p").toogle();
+    })
+
+    $("#tog").click(function () {	 
+        $('#tarjeta').toggle();
+        });
+
+    $("button").click(function(){
+        $("p").toggle();
+        });
+
+});
